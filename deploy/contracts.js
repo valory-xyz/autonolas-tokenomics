07a8c47b6575e10597626b2dbf22fbed40ea0ee9
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2021-12-27 16:42:10 +0700

    Writing initial setup date (addresses) into the JSON file

--- deploy/contracts.js
+++ deploy/contracts.js
@@ -5,16 +5,19 @@ module.exports = async () => {
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
+    // Deploying component registry
     const ComponentRegistry = await ethers.getContractFactory("ComponentRegistry");
     const componentRegistry = await ComponentRegistry.deploy("agent components", "MECHCOMP",
         "https://localhost/component/");
     await componentRegistry.deployed();
 
+    // Deploying agent registry
     const AgentRegistry = await ethers.getContractFactory("AgentRegistry");
     const agentRegistry = await AgentRegistry.deploy("agent", "MECH", "https://localhost/agent/",
         componentRegistry.address);
     await agentRegistry.deployed();
 
+    // Deploying minter
     const MechMinter = await ethers.getContractFactory("MechMinter");
     const mechMinter = await MechMinter.deploy(componentRegistry.address, agentRegistry.address, "mech minter",
         "MECHMINTER");
@@ -24,7 +27,18 @@ module.exports = async () => {
     console.log("AgentRegistry deployed to:", agentRegistry.address);
     console.log("MechMinter deployed to:", mechMinter.address);
 
+    // Whitelisting minter in component and agent registry
     await componentRegistry.changeMinter(mechMinter.address);
     await agentRegistry.changeMinter(mechMinter.address);
     console.log("Whitelisted MechMinter addresses to both ComponentRegistry and AgentRegistry contract instances");
+
+    // Writing the JSON with the initial deployment data
+    let initDeployJSON = {
+      componentRegistry: componentRegistry.address,
+      agentRegistry: agentRegistry.address,
+      mechMinter: mechMinter.address
+    };
+
+    let fs = require('fs');
+    fs.writeFileSync("initDeploy.json", JSON.stringify(initDeployJSON));
 };
