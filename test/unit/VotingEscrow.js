4e4700099eeeb6ceb69349c85b8d074ba967bfe1
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-02-17 19:32:41 +0000

    fix: accounting for linter fixes

--- test/unit/VotingEscrow.js
+++ test/unit/VotingEscrow.js
@@ -7,12 +7,7 @@ describe("VotingEscrow", function () {
     let token;
     let ve;
     let signers;
-    const description = "description";
-    const componentHashes = [{hash: "0x" + "0".repeat(64), hashFunction: "0x12", size: "0x20"},
-        {hash: "0x" + "1".repeat(64), hashFunction: "0x12", size: "0x20"}];
-    const agentHashes = [{hash: "0x" + "5".repeat(64), hashFunction: "0x12", size: "0x20"},
-        {hash: "0x" + "6".repeat(64), hashFunction: "0x12", size: "0x20"}];
-    const dependencies = [];
+
     beforeEach(async function () {
         const Token = await ethers.getContractFactory("veOLA");
         token = await Token.deploy();
@@ -37,7 +32,7 @@ describe("VotingEscrow", function () {
             expect(await ve.balanceOf(owner.address)).to.equal(0);
             await ve.create_lock(ethers.utils.parseEther("1"), lockDuration);
             const balance = await ve.balanceOf(owner.address);
-//            console.log(balance);
+            console.log(balance);
         });
     });
 
