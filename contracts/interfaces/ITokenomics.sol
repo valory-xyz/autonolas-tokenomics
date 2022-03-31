d96dfa49ae6a7eaaecc1501080ed411a95fcef2e
AL <info@77ph.net>
2022-03-31 12:43:37 -0500

    feat and refactor: first version of economics factors

---
+++ contracts/interfaces/ITokenomics.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @dev Interface for tokenomics management.
+interface ITokenomics {
+    function getDF(uint256 epoch) external view returns (uint256 df);
+    function getDForCalc(uint256 _epoch) external returns (uint256 df);
+    function getEpochLen() external view returns (uint256);
+}
