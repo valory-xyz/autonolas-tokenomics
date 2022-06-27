035027fb9fdee6b0a5b5e54eb751b60b9f5700f1
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2022-06-27 09:56:39 +0100

    chore: bumping solidity version to 0.8.15

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,7 +26,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.14",
+                version: "0.8.15",
                 settings: {
                     optimizer: {
                         enabled: true,
