293e421696a2659cbd8c51f0014257a22e6be96a
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-02-02 11:29:19 +0000

    chore: setting a higher compilation optimization

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,7 +34,7 @@ module.exports = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 1000,
+                        runs: 4000,
                     },
                 },
             },
