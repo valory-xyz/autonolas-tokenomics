67d3b0861767edf20e9eb7cdc44aff041e02b7a4
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-02-06 12:27:18 +0000

    test: correcting foundry tests

--- test/Treasury.t.sol
+++ test/Treasury.t.sol
@@ -37,7 +37,7 @@ contract BaseSetup is Test {
             address(deployer), epochLen, address(deployer), address(deployer), address(deployer), address(0));
         // Depository contract is irrelevant here, so we are using a deployer's address
         // Dispenser address is irrelevant in these tests, so its contract is passed as a zero address
-        treasury = new Treasury(address(olas), address(tokenomics), deployer, address(0));
+        treasury = new Treasury(address(olas), address(tokenomics), deployer, deployer);
 
         // Change to the correct treasury address
         tokenomics.changeManagers(address(treasury), address(0), address(0));
