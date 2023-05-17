9f83f3c49050375e5b759b0764b532d2e0c73ed6
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-05-17 13:16:11 +0100

    feat and test: continue with mock tokenomics merkle

--- contracts/test/MockTokenomicsMerkle.sol
+++ contracts/test/MockTokenomicsMerkle.sol
@@ -5,6 +5,14 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 error WrongLength(uint256 length1, uint256 length2);
 
+error AlreadyClaimed(uint256 roundId, uint256 unitId);
+
+error ZeroValue();
+
+error InsufficientBalance(uint256 requested, uint256 available);
+
+error ClaimProofFailed(uint256 roundId);
+
 struct MultiProof {
     bytes32[] merkleProof;
     bool[] proofFlags;
@@ -17,19 +25,23 @@ struct RoundInfo {
 
 /// @title MockTokenomicsMerkle - Smart contract for mocking the tokenomics based on Merkle proofs
 contract MockTokenomicsMerkle {
+    event Donation(address indexed sender, uint256 amount, uint256 indexed rountId, bytes32 merkleRoot, bytes32 hashIPFS);
+    event Claim(address indexed sender, uint256 roundId, uint256[] unitIds, uint256[] amounts);
+
     uint256 public roundCounter;
     mapping(uint256 => RoundInfo) public mapRoundInfo;
     mapping(uint256 => uint256) public mapUnitAmounts;
+    mapping(uint256 => mapping(uint256 => bool)) public mapClaimedRoundUnits;
     
     receive() external payable {
     }
 
-    function donate(bytes32 merkleRoot) external payable {
-        uint256 rCounter = roundCounter;
-        RoundInfo memory rInfo = RoundInfo(merkleRoot, msg.value);
-        mapRoundInfo[rCounter] = rInfo;
-        rCounter++;
-        roundCounter = rCounter;
+    function donate(bytes32 merkleRoot, bytes32 hashIPFS) external payable returns (uint256 roundId) {
+        roundId = roundCounter;
+        mapRoundInfo[roundId] = RoundInfo(merkleRoot, msg.value);
+        roundCounter = roundId + 1;
+
+        emit Donation(msg.sender, msg.value, roundId, merkleRoot, hashIPFS);
     }
 
     function claim(
@@ -37,17 +49,27 @@ contract MockTokenomicsMerkle {
         uint256[] memory unitIds,
         uint256[] memory amounts,
         MultiProof calldata multiProof
-    ) external view returns (bool valid)
+    ) external returns (bool valid)
     {
         uint256 uLength = unitIds.length;
 
-        if (unitIds.length != amounts.length) {
+        if (unitIds.length == 0 || unitIds.length != amounts.length) {
             revert WrongLength(uLength, amounts.length);
         }
 
         bytes32[] memory leaves = new bytes32[](uLength);
         for (uint256 i = 0; i < uLength; ++i) {
-            // double hashed for better resistance to (second) pre-image attacks
+            // Check for the claimed round
+            if (mapClaimedRoundUnits[roundId][unitIds[i]]) {
+                revert AlreadyClaimed(roundId, unitIds[i]);
+            }
+
+            // Check for the zero amount
+            if (amounts[i] == 0) {
+                revert ZeroValue();
+            }
+
+            // Double hashed for better resistance to (second) pre-image attacks
             bytes32 leaf = keccak256(
                 abi.encode(
                     keccak256(abi.encode(unitIds[i], amounts[i]))
@@ -55,7 +77,25 @@ contract MockTokenomicsMerkle {
             );
             leaves[i] = leaf;
         }
+
         valid = MerkleProof.multiProofVerifyCalldata(multiProof.merkleProof, multiProof.proofFlags,
             mapRoundInfo[roundId].merkleRoot, leaves);
+
+        if (valid) {
+            uint256 amount = mapRoundInfo[roundId].amount;
+            for (uint256 i = 0; i < uLength; ++i) {
+                // Check for the balance left
+                if (amounts[i] > amount) {
+                    revert InsufficientBalance(amounts[i], amount);
+                }
+                amount -= amounts[i];
+                mapClaimedRoundUnits[roundId][unitIds[i]] = true;
+            }
+            mapRoundInfo[roundId].amount = amount;
+        } else {
+            revert ClaimProofFailed(roundId);
+        }
+
+        emit Claim(msg.sender, roundId, unitIds, amounts);
     }
 }
---
+++ test/MockTokenomicsMerkle.js
@@ -0,0 +1,115 @@
+/*global describe, beforeEach, it, context*/
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
+
+describe("Tokenomics Merkle", async () => {
+    const AddressZero = "0x" + "0".repeat(40);
+    const defaultHashIPSF = "0x" + "0".repeat(64);
+
+    let signers;
+    let deployer;
+    let tokenomicsMerkle;
+
+    // These should not be in beforeEach.
+    beforeEach(async () => {
+        signers = await ethers.getSigners();
+        deployer = signers[0];
+
+        const TokenomicsMerkle = await ethers.getContractFactory("MockTokenomicsMerkle");
+        tokenomicsMerkle = await TokenomicsMerkle.deploy();
+        await tokenomicsMerkle.deployed();
+    });
+
+    context("Merkle proofs", async function () {
+        it("Donate and claim", async function () {
+            const donate = [[1, 100], [2, 200], [3, 300]];
+
+            const merkleTree = StandardMerkleTree.of(donate, ["uint256", "uint256"]);
+
+            // Make a donation
+            let donationAmount = 0;
+            for (let i = 0; i < donate.length; i++) {
+                donationAmount += donate[i][1];
+            }
+            const roundId = await tokenomicsMerkle.callStatic.donate(merkleTree.root, defaultHashIPSF, {value: donationAmount});
+            await tokenomicsMerkle.donate(merkleTree.root, defaultHashIPSF, {value: donationAmount});
+
+            // Claim for unit 1 and 2
+            const proofStruct = merkleTree.getMultiProof([0, 1]);
+
+            const unitIds = [donate[0][0], donate[1][0]];
+            const amounts = [donate[0][1], donate[1][1]];
+            const multiProof = {merkleProof: proofStruct.proof, proofFlags: proofStruct.proofFlags};
+            await tokenomicsMerkle.claim(roundId, unitIds, amounts, multiProof);
+
+            // Try to claim same amounts again
+            await expect(
+                tokenomicsMerkle.claim(roundId, unitIds, amounts, multiProof)
+            ).to.be.revertedWithCustomError(tokenomicsMerkle, "AlreadyClaimed");
+        });
+
+        it("Donate less and try to claim more", async function () {
+            const donate = [[1, 100], [2, 200], [3, 300]];
+
+            const merkleTree = StandardMerkleTree.of(donate, ["uint256", "uint256"]);
+
+            // Make a donation
+            let donationAmount = 0;
+            for (let i = 0; i < donate.length; i++) {
+                donationAmount += donate[i][1];
+            }
+            const roundId = await tokenomicsMerkle.callStatic.donate(merkleTree.root, defaultHashIPSF, {value: 1});
+            await tokenomicsMerkle.donate(merkleTree.root, defaultHashIPSF, {value: 1});
+
+            // Try to claim for unit 1 and 2
+            const proofStruct = merkleTree.getMultiProof([0, 1]);
+
+            const unitIds = [donate[0][0], donate[1][0]];
+            const amounts = [donate[0][1], donate[1][1]];
+            const multiProof = {merkleProof: proofStruct.proof, proofFlags: proofStruct.proofFlags};
+            await expect(
+                tokenomicsMerkle.claim(roundId, unitIds, amounts, multiProof)
+            ).to.be.revertedWithCustomError(tokenomicsMerkle, "InsufficientBalance");
+        });
+
+        it("Donate and try to claim with wrong proofs", async function () {
+            const donations = [[[1, 100], [2, 200], [3, 300]], [[1, 600], [2, 500], [3, 400]]];
+
+            const merkleTrees = new Array();
+            merkleTrees.push(StandardMerkleTree.of(donations[0], ["uint256", "uint256"]));
+            merkleTrees.push(StandardMerkleTree.of(donations[1], ["uint256", "uint256"]));
+
+            // Make two donations
+            const donationAmounts = new Array(2).fill(0);
+            for (let j = 0; j < donations.length; j++) {
+                for (let i = 0; i < donations[j].length; i++) {
+                    donationAmounts[j] += donations[j][i][1];
+                }
+            }
+            const roundIds = [0, 1];
+            await tokenomicsMerkle.donate(merkleTrees[0].root, defaultHashIPSF, {value: donationAmounts[0]});
+            await tokenomicsMerkle.donate(merkleTrees[1].root, defaultHashIPSF, {value: donationAmounts[1]});
+
+            // Try to claim for unit 1 and 2
+            const proofStructs = [merkleTrees[0].getMultiProof([0, 1]), merkleTrees[1].getMultiProof([0, 1])];
+
+            const unitIds = [[donations[0][0][0], donations[0][1][0]], [donations[1][0][0], donations[1][1][0]]];
+            const amounts = [[donations[0][0][1], donations[0][1][1]], [donations[1][0][1], donations[1][1][1]]];
+            const multiProofs = [{merkleProof: proofStructs[0].proof, proofFlags: proofStructs[0].proofFlags},
+                {merkleProof: proofStructs[1].proof, proofFlags: proofStructs[1].proofFlags}];
+
+            // Try to claim from different rounds
+            await expect(
+                tokenomicsMerkle.claim(roundIds[1], unitIds[0], amounts[0], multiProofs[0])
+            ).to.be.revertedWithCustomError(tokenomicsMerkle, "ClaimProofFailed");
+            await expect(
+                tokenomicsMerkle.claim(roundIds[0], unitIds[1], amounts[1], multiProofs[1])
+            ).to.be.revertedWithCustomError(tokenomicsMerkle, "ClaimProofFailed");
+
+            // Claim with correct proofs
+            await tokenomicsMerkle.claim(roundIds[0], unitIds[0], amounts[0], multiProofs[0]);
+            await tokenomicsMerkle.claim(roundIds[1], unitIds[1], amounts[1], multiProofs[1]);
+        });
+    });
+});
