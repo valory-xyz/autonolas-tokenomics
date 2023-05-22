9045a9c99cef352482fa441f739027e0f07c3de0
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-05-22 15:16:59 +0100

    chore: adjusting tests, workflow

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,15 @@
 module.exports = {
-    //skipFiles: ["test/ERC20Token.sol", "test/AttackDeposit.sol"]
+    skipFiles: [
+        "test/ERC20Token.sol",
+        "test/DepositAttacker.sol",
+        "test/FlashLoanAttacker.sol",
+        "test/MockRegistry.sol",
+        "test/MockTokenomics.sol",
+        "test/MockVE.sol",
+        "test/ReentrancyAttacker.sol",
+        "test/TestTokenomics.sol",
+        "test/UniswapFactory.sol",
+        "test/UniswapRouter.sol",
+        "test/Zuniswapv2.sol"
+    ]
 };
