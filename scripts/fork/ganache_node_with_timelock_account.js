aec97b7264b6bef1986c0ff4c35f7b7a316e1d1f
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-07-14 18:18:46 +0100

    chore: expanding on a bonding product script with fork node interaction

--- scripts/fork/ganache_node_with_timelock_account.js
+++ scripts/fork/ganache_node_with_timelock_account.js
@@ -21,13 +21,15 @@ async function main() {
     const timelockAddress = parsedData.timelockAddress;
     const treasuryAddress = parsedData.treasuryAddress;
     const depositoryAddress = parsedData.depositoryAddress;
-    const token = parsedData.OLAS_ETH_PairAddress;
+    const tokenAddress = parsedData.OLAS_ETH_PairAddress;
+    const olasAddress = parsedData.olasAddress;
+    const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
     // Timelock address is specified via the "-u" command to ganache node
     const signer = provider.getSigner(timelockAddress);
 
-    //let privateKey = "PRIVATE_KEY";
-    //let wallet = new ethers.Wallet(privateKey, provider);
+    let privateKey = process.env.PRIVATE_KEY;
+    let wallet = new ethers.Wallet(privateKey, provider);
     //wallet.sendTransaction({to: timelockAddress, value: ethers.utils.parseEther("1")});
 
     const treasuryJSON = "artifacts/contracts/Treasury.sol/Treasury.json";
@@ -37,11 +39,11 @@ async function main() {
 
     // Treasury contract instance
     const treasury = new ethers.Contract(treasuryAddress, abi, signer);
-    console.log(treasury.address);
+    //console.log(treasury.address);
 
-    // Enable token
-    //await treasury.connect(signer).enableToken(token);
-    //console.log(await treasury.isEnabled(token));
+    // Enable tokenAddress
+    //await treasury.connect(signer).enableToken(tokenAddress);
+    //console.log(await treasury.isEnabled(tokenAddress));
 
     const depositoryJSON = "artifacts/contracts/Depository.sol/Depository.json";
     contractFromJSON = fs.readFileSync(depositoryJSON, "utf8");
@@ -50,13 +52,74 @@ async function main() {
 
     // Depository contract instance
     const depository = new ethers.Contract(depositoryAddress, abi, signer);
-    console.log(depository.address);
+    //console.log(depository.address);
 
     // Create a bonding product
-    //const vesting = 3600 * 24 * 7;
-    //await depository.connect(signer).create(token, "229846666583294163442", "1" + "0".repeat(24), vesting);
+    const vesting = 3600 * 24 * 7;
+    //await depository.connect(signer).create(tokenAddress, "229846666583294163442", "1" + "0".repeat(24), vesting);
+    //console.log(await depository.isActiveProduct(0));
 
-    console.log(await depository.isActiveProduct(0));
+    // Get the Uniswap Router contract
+    const uniswapRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const uniswapRouterJSON = "artifacts/@uniswap/v2-periphery/contracts/UniswapV2Router02.sol/UniswapV2Router02.json";
+    contractFromJSON = fs.readFileSync(uniswapRouterJSON, "utf8");
+    parsedFile = JSON.parse(contractFromJSON);
+    abi = parsedFile["abi"];
+    const router = new ethers.Contract(uniswapRouterAddress, abi, signer);
+
+    // Get OLAS tokens
+    // to is the Metamask address
+    const to = wallet.address;
+    const deadline = Math.floor(Date.now() / 1e3 + vesting);
+    console.log(deadline);
+    // WETH-OLAS array
+    const path = [wethAddress, olasAddress];
+    // Swap ETH for OLAS tokens
+    //await router.connect(wallet).swapExactETHForTokens(0, path, to, deadline, {value: ethers.utils.parseEther("1")});
+
+    // Get the OLAS contract
+    const olasJSON = "abis/test/OLAS.json";
+    contractFromJSON = fs.readFileSync(olasJSON, "utf8");
+    parsedFile = JSON.parse(contractFromJSON);
+    abi = parsedFile["abi"];
+    const olas = new ethers.Contract(olasAddress, abi, signer);
+    //console.log("OLAS balance:", Number(await olas.balanceOf(to)));
+
+    // Get the WETH contract
+    const wethJSON = "artifacts/canonical-weth/contracts/WETH9.sol/WETH9.json";
+    contractFromJSON = fs.readFileSync(wethJSON, "utf8");
+    parsedFile = JSON.parse(contractFromJSON);
+    abi = parsedFile["abi"];
+    const weth = new ethers.Contract(wethAddress, abi, signer);
+
+    // Get WETH for ETH
+    //await weth.connect(wallet).deposit({value: ethers.utils.parseEther("1")});
+    //console.log("WETH balance:", Number(await weth.balanceOf(wallet.address)));
+
+    // Approve tokens for router
+    //await olas.connect(wallet).approve(uniswapRouterAddress, ethers.constants.MaxUint256);
+    //await weth.connect(wallet).approve(uniswapRouterAddress, ethers.constants.MaxUint256);
+    // Get WETH-OLAS liquidity
+    //await router.connect(wallet).addLiquidity(wethAddress, olasAddress, ethers.utils.parseEther("0.5"), ethers.utils.parseEther("10000"), 0, 0, to, deadline);
+
+    // Get the LP contract
+    const pairJSON = "artifacts/@uniswap/v2-core/contracts/UniswapV2Pair.sol/UniswapV2Pair.json";
+    contractFromJSON = fs.readFileSync(pairJSON, "utf8");
+    parsedFile = JSON.parse(contractFromJSON);
+    abi = parsedFile["abi"];
+    const pair = new ethers.Contract(tokenAddress, abi, signer);
+    const tokenBalance = Number(await pair.balanceOf(to));
+    const totalSupply = Number(await pair.totalSupply());
+    console.log("Our share", (tokenBalance * 1.0 / totalSupply));
+
+    // Approve LP token for treasury
+    //await pair.approve(treasuryAddress, ethers.constants.MaxUint256);
+
+    // Deposit for the bond
+    //await depository.connect(wallet).deposit(0, "5" + "0".repeat(19));
+
+    // Get the current nonce
+    //console.log(await provider.getTransactionCount(to));
 }
 
 main()
