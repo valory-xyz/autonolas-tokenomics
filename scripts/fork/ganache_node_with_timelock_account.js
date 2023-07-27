b975397906d14091e3af052de1983fe582d36324
AL <info@77ph.net>
2023-07-27 15:12:47 +0300

    chore: proof addLiqidity and removeLiqidity in fork script

--- scripts/fork/ganache_node_with_timelock_account.js
+++ scripts/fork/ganache_node_with_timelock_account.js
@@ -84,9 +84,9 @@ async function main() {
         topics: filter,
     });
 
-    const blockNumber = logs[0].blockNumber;
-    const block = await provider.getBlock(blockNumber);
-    const timestamp = block.timestamp;
+    //const blockNumber = logs[0].blockNumber;
+    //const block = await provider.getBlock(blockNumber);
+    //const timestamp = block.timestamp;
     //console.log(timestamp);
 
     // Get the Uniswap Router contract
@@ -105,7 +105,7 @@ async function main() {
     // WETH-OLAS array
     const path = [wethAddress, olasAddress];
     // Swap ETH for OLAS tokens
-    //await router.connect(wallet).swapExactETHForTokens(0, path, to, deadline, {value: ethers.utils.parseEther("1")});
+    await router.connect(wallet).swapExactETHForTokens(0, path, to, deadline, {value: ethers.utils.parseEther("1")});
 
     // Get the OLAS contract
     const olasJSON = "abis/test/OLAS.json";
@@ -113,7 +113,7 @@ async function main() {
     parsedFile = JSON.parse(contractFromJSON);
     abi = parsedFile["abi"];
     const olas = new ethers.Contract(olasAddress, abi, signer);
-    //console.log("OLAS balance:", Number(await olas.balanceOf(to)));
+    console.log("OLAS balance:", Number(await olas.balanceOf(to)));
 
     // Get the WETH contract
     const wethJSON = "artifacts/canonical-weth/contracts/WETH9.sol/WETH9.json";
@@ -123,27 +123,77 @@ async function main() {
     const weth = new ethers.Contract(wethAddress, abi, signer);
 
     // Get WETH for ETH
-    //await weth.connect(wallet).deposit({value: ethers.utils.parseEther("1")});
-    //console.log("WETH balance:", Number(await weth.balanceOf(wallet.address)));
+    await weth.connect(wallet).deposit({value: ethers.utils.parseEther("1")});
+    console.log("WETH balance:", Number(await weth.balanceOf(wallet.address)));
 
     // Approve tokens for router
-    //await olas.connect(wallet).approve(uniswapRouterAddress, ethers.constants.MaxUint256);
-    //await weth.connect(wallet).approve(uniswapRouterAddress, ethers.constants.MaxUint256);
-    // Get WETH-OLAS liquidity
-    //await router.connect(wallet).addLiquidity(wethAddress, olasAddress, ethers.utils.parseEther("0.5"), ethers.utils.parseEther("10000"), 0, 0, to, deadline);
-
+    await olas.connect(wallet).approve(uniswapRouterAddress, ethers.constants.MaxUint256);
+    await weth.connect(wallet).approve(uniswapRouterAddress, ethers.constants.MaxUint256);
+    
     // Get the LP contract
     const pairJSON = "artifacts/@uniswap/v2-core/contracts/UniswapV2Pair.sol/UniswapV2Pair.json";
     contractFromJSON = fs.readFileSync(pairJSON, "utf8");
     parsedFile = JSON.parse(contractFromJSON);
     abi = parsedFile["abi"];
     const pair = new ethers.Contract(tokenAddress, abi, signer);
-    const tokenBalance = Number(await pair.balanceOf(to));
-    const totalSupply = Number(await pair.totalSupply());
-    console.log("Our share", (tokenBalance * 1.0 / totalSupply));
+    await pair.connect(wallet).approve(uniswapRouterAddress, ethers.constants.MaxUint256);
+    const e18 = ethers.BigNumber.from("1" + "0".repeat(18));
+    let reserves = await pair.getReserves();
+    let reservesOLAS = reserves._reserve0;
+    let reservesETH = reserves._reserve1;
+    let totalSupply = await pair.totalSupply();
+    let priceLPBefore = (reservesOLAS.mul(e18)).div(totalSupply);
+    console.log("priceLP",priceLPBefore);
+    // Get WETH-OLAS liquidity
+    await router.connect(wallet).addLiquidity(wethAddress, olasAddress, ethers.utils.parseEther("0.5"), ethers.utils.parseEther("10000"), 0, 0, to, deadline);
+  
+
+    let tokenBalance = await pair.balanceOf(to);
+    totalSupply = await pair.totalSupply();
+    console.log("Our share", (Number(tokenBalance) * 1.0 / Number(totalSupply)));
+    reserves = await pair.getReserves();
+    reservesOLAS = reserves._reserve0;
+    reservesETH = reserves._reserve1;
+    let priceLPAfterAdd = (reservesOLAS.mul(e18)).div(totalSupply);
+    console.log("priceLP",priceLPAfterAdd);
+    if(priceLPBefore.eq(priceLPAfterAdd)) {
+        console.log("addLiqidity not moved priceLP = reservesOLAS/totalSupply");
+    }
+    tokenBalance = tokenBalance.div(ethers.BigNumber.from(2));
+    console.log(tokenBalance);
+
+
+    await router.connect(wallet).removeLiquidity(wethAddress, olasAddress, tokenBalance, 0, 0, to, deadline);
+
+    reserves = await pair.getReserves();
+    reservesOLAS = reserves._reserve0;
+    reservesETH = reserves._reserve1;
+    totalSupply = await pair.totalSupply();
+    let priceLPAfterRem = (reservesOLAS.mul(e18)).div(totalSupply);
+    console.log("priceLP",priceLPAfterRem);
+
+    if(priceLPAfterRem.eq(priceLPAfterAdd)) {
+        console.log("removeLiqidity not moved priceLP = reservesOLAS/totalSupply");
+    }
+
+    /*
+    node scripts/fork/ganache_node_with_timelock_account.js
+Current fork block number: 17784307
+1691064650
+OLAS balance: 1.0629334248726272e+22
+WETH balance: 1000000000000000000
+priceLP BigNumber { _hex: '0x05b819057dd47a6b2f', _isBigNumber: true }
+Our share 0.00356481863355418
+priceLP BigNumber { _hex: '0x05b819057dd47a6b2f', _isBigNumber: true }
+addLiqidity not moved priceLP = reservesOLAS/totalSupply
+BigNumber { _hex: '0x015c195520dc3c6cc1', _isBigNumber: true }
+priceLP BigNumber { _hex: '0x05b819057dd47a6b2f', _isBigNumber: true }
+removeLiqidity not moved priceLP = reservesOLAS/totalSupply
+2925
+    */
 
     // Approve LP token for treasury
-    //await pair.connect(wallet).approve(treasuryAddress, ethers.constants.MaxUint256);
+    await pair.connect(wallet).approve(treasuryAddress, ethers.constants.MaxUint256);
 
     // Deposit for the bond
     //await depository.connect(wallet).deposit(0, "5" + "0".repeat(19));
