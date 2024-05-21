8f24a9eb1f0965d9701602775fb338bf0261a374
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2024-05-22 00:13:45 +0100

    refactor: updating inflation numbers

--- contracts/TokenomicsConstants.sol
+++ contracts/TokenomicsConstants.sol
@@ -34,15 +34,15 @@ abstract contract TokenomicsConstants {
         // For the first 10 years the supply caps are pre-defined
         if (numYears < 10) {
             uint96[10] memory supplyCaps = [
-                529_659_000_00e16,
-                569_913_084_00e16,
-                641_152_219_50e16,
-                708_500_141_72e16,
-                771_039_876_00e16,
-                828_233_282_97e16,
-                879_860_040_11e16,
-                925_948_139_65e16,
-                966_706_331_40e16,
+                529_659_000e18,
+                569_913_084e18,
+                610_313_084e18,
+                666_313_084e18,
+                746_313_084e18,
+                818_313_084e18,
+                882_313_084e18,
+                930_313_084e18,
+                970_313_084e18,
                 1_000_000_000e18
             ];
             supplyCap = supplyCaps[numYears];
@@ -71,16 +71,16 @@ abstract contract TokenomicsConstants {
         if (numYears < 10) {
             // Initial OLAS allocation is 526_500_000_0e17
             uint88[10] memory inflationAmounts = [
-                3_159_000_00e16,
-                40_254_084_00e16,
-                71_239_135_50e16,
-                67_347_922_22e16,
-                62_539_734_28e16,
-                57_193_406_97e16,
-                51_626_757_14e16,
-                46_088_099_54e16,
-                40_758_191_75e16,
-                33_293_668_60e16
+                3_159_000e18,
+                40_254_084e18,
+                40_400_000e18,
+                56_000_000e18,
+                80_000_000e18,
+                72_000_000e18,
+                64_000_000e18,
+                48_000_000e18,
+                40_000_000e18,
+                29_686_916e18
             ];
             inflationAmount = inflationAmounts[numYears];
         } else {
