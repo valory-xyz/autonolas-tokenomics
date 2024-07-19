e9945cd0dd6c8c0dc0fefda76d7f60e7ef56511b
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2024-07-19 20:09:07 +0100

    chore: staking deployment scripts

---
+++ scripts/deployment/staking/deploy_09_set_targer_dispensers.js
@@ -0,0 +1,81 @@
+/*global process*/
+
+const { ethers } = require("hardhat");
+const { LedgerSigner } = require("@anders-t/ethers-ledger");
+
+async function main() {
+    const fs = require("fs");
+    const globalsFile = "globals.json";
+    const dataFromJSON = fs.readFileSync(globalsFile, "utf8");
+    let parsedData = JSON.parse(dataFromJSON);
+    const useLedger = parsedData.useLedger;
+    const derivationPath = parsedData.derivationPath;
+    const providerName = parsedData.providerName;
+    const arbitrumDepositProcessorL1Address = parsedData.arbitrumDepositProcessorL1Address;
+    const arbitrumTargetDispenserL2Address = parsedData.arbitrumTargetDispenserL2Address;
+    const baseDepositProcessorL1Address = parsedData.baseDepositProcessorL1Address;
+    const baseTargetDispenserL2Address = parsedData.baseTargetDispenserL2Address;
+    const celoDepositProcessorL1Address = parsedData.celoDepositProcessorL1Address;
+    const celoTargetDispenserL2Address = parsedData.celoTargetDispenserL2Address;
+    const gnosisDepositProcessorL1Address = parsedData.gnosisDepositProcessorL1Address;
+    const gnosisTargetDispenserL2Address = parsedData.gnosisTargetDispenserL2Address;
+    const optimismDepositProcessorL1Address = parsedData.optimismDepositProcessorL1Address;
+    const optimismTargetDispenserL2Address = parsedData.optimismTargetDispenserL2Address;
+    const polygonDepositProcessorL1Address = parsedData.polygonDepositProcessorL1Address;
+    const polygonTargetDispenserL2Address = parsedData.polygonTargetDispenserL2Address;
+    let EOA;
+
+    const provider = await ethers.providers.getDefaultProvider(providerName);
+    const signers = await ethers.getSigners();
+
+    if (useLedger) {
+        EOA = new LedgerSigner(provider, derivationPath);
+    } else {
+        EOA = signers[0];
+    }
+    // EOA address
+    const deployer = await EOA.getAddress();
+    console.log("EOA is:", deployer);
+
+    // Get all the contracts
+    const arbitrumDepositProcessorL1 = await ethers.getContractAt("ArbitrumDepositProcessorL1", arbitrumDepositProcessorL1Address);
+    const baseDepositProcessorL1 = await ethers.getContractAt("OptimismDepositProcessorL1", baseDepositProcessorL1Address);
+    const celoDepositProcessorL1 = await ethers.getContractAt("WormholeDepositProcessorL1", celoDepositProcessorL1Address);
+    const gnosisDepositProcessorL1 = await ethers.getContractAt("GnosisDepositProcessorL1", gnosisDepositProcessorL1Address);
+    const optimismDepositProcessorL1 = await ethers.getContractAt("OptimismDepositProcessorL1", optimismDepositProcessorL1Address);
+    const polygonDepositProcessorL1 = await ethers.getContractAt("PolygonDepositProcessorL1", polygonDepositProcessorL1);
+
+    // Transaction signing and execution
+    console.log("9. EOA to set TargetDispenserL2 in DepositProcessorL1");
+
+    console.log("You are signing the following transaction: ArbitrumDepositProcessorL1.connect(EOA).setL2TargetDispenser()");
+    let result = await arbitrumDepositProcessorL1.connect(EOA).setL2TargetDispenser(arbitrumTargetDispenserL2Address);
+    console.log("Transaction:", result.hash);
+
+    console.log("You are signing the following transaction: OptimismDepositProcessorL1.connect(EOA).setL2TargetDispenser()");
+    result = await baseDepositProcessorL1.connect(EOA).setL2TargetDispenser(baseTargetDispenserL2Address);
+    console.log("Transaction:", result.hash);
+
+    console.log("You are signing the following transaction: WormholeDepositProcessorL1.connect(EOA).setL2TargetDispenser()");
+    result = await celoDepositProcessorL1.connect(EOA).setL2TargetDispenser(celoTargetDispenserL2Address);
+    console.log("Transaction:", result.hash);
+
+    console.log("You are signing the following transaction: GnosisDepositProcessorL1.connect(EOA).setL2TargetDispenser()");
+    result = await gnosisDepositProcessorL1.connect(EOA).setL2TargetDispenser(gnosisTargetDispenserL2Address);
+    console.log("Transaction:", result.hash);
+
+    console.log("You are signing the following transaction: OptimismDepositProcessorL1.connect(EOA).setL2TargetDispenser()");
+    result = await optimismDepositProcessorL1.connect(EOA).setL2TargetDispenser(optimismTargetDispenserL2Address);
+    console.log("Transaction:", result.hash);
+
+    console.log("You are signing the following transaction: PolygonDepositProcessorL1.connect(EOA).setL2TargetDispenser()");
+    result = await polygonDepositProcessorL1.connect(EOA).setL2TargetDispenser(polygonTargetDispenserL2Address);
+    console.log("Transaction:", result.hash);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
---
+++ scripts/proposals/proposal_09_change_dispenser_and_set_deposit_processors.js
@@ -0,0 +1,63 @@
+/*global process*/
+
+const { ethers } = require("hardhat");
+
+async function main() {
+    const fs = require("fs");
+    const globalsFile = "globals.json";
+    const dataFromJSON = fs.readFileSync(globalsFile, "utf8");
+    let parsedData = JSON.parse(dataFromJSON);
+    const providerName = parsedData.providerName;
+
+    const provider = await ethers.providers.getDefaultProvider(providerName);
+
+    // Get all the necessary contract addresses
+    const tokenomicsProxyAddress = parsedData.tokenomicsProxyAddress;
+    const treasuryAddress = parsedData.treasuryAddress;
+    const dispenserAddress = parsedData.dispenserAddress;
+    const arbitrumDepositProcessorL1Address = parsedData.arbitrumDepositProcessorL1Address;
+    const baseDepositProcessorL1Address = parsedData.baseDepositProcessorL1Address;
+    const celoDepositProcessorL1Address = parsedData.celoDepositProcessorL1Address;
+    const ethereumDepositProcessorAddress = parsedData.ethereumDepositProcessorAddress;
+    const gnosisDepositProcessorL1Address = parsedData.gnosisDepositProcessorL1Address;
+    const optimismDepositProcessorL1Address = parsedData.optimismDepositProcessorL1Address;
+    const polygonDepositProcessorL1Address = parsedData.polygonDepositProcessorL1Address;
+    const minStakingWeight = parsedData.minStakingWeight;
+    const maxStakingIncentive = parsedData.maxStakingIncentive;
+
+    // Get contract instances
+    const tokenomics = await ethers.getContractAt("Tokenomics", tokenomicsProxyAddress);
+    const treasury = await ethers.getContractAt("Treasury", treasuryAddress);
+    const dispenser = await ethers.getContractAt("Dispenser", dispenserAddress);
+
+    const AddressZero = ethers.constants.AddressZero;
+
+    // Proposal preparation
+    console.log("Proposal 9. Change dispenser address in tokenomics and treasury, manage deposit processors");
+    const targets = [tokenomicsProxyAddress, tokenomicsProxyAddress, treasuryAddress, dispenserAddress];
+    const values = [0, 0, 0, 0];
+    const callDatas = [
+        tokenomics.interface.encodeFunctionData("changeManagers", [AddressZero, AddressZero, dispenserAddress]),
+        tokenomics.interface.encodeFunctionData("changeStakingParams", [maxStakingIncentive, minStakingWeight]),
+        treasury.interface.encodeFunctionData("changeManagers", [AddressZero, AddressZero, dispenserAddress]),
+        dispenser.interface.encodeFunctionData([arbitrumDepositProcessorL1Address, baseDepositProcessorL1Address,
+            celoDepositProcessorL1Address, ethereumDepositProcessorAddress, gnosisDepositProcessorL1Address,
+            optimismDepositProcessorL1Address, polygonDepositProcessorL1Address],
+            [42161, 8453, 42220, 1, 100, 10, 137])
+    ];
+
+    const description = "Change Dispenser address in Tokenomics and Treasury, manage deposit processors";
+
+    // Proposal details
+    console.log("targets:", targets);
+    console.log("values:", values);
+    console.log("call datas:", callDatas);
+    console.log("description:", description);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
