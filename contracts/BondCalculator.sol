dc18a3dc7a57933d4c3a9387d8b066333c20e9ce
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2024-07-22 12:49:04 +0100

    chore: remove unused import

--- contracts/BondCalculator.sol
+++ contracts/BondCalculator.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import {mulDiv} from "@prb/math/src/Common.sol";
 import {GenericBondCalculator} from "./GenericBondCalculator.sol";
 import {IVotingEscrow} from "./interfaces/IVotingEscrow.sol";
 
