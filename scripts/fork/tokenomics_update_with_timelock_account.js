f32f0226a7dc60ae5ac2f177aa7419aba038f1d3
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-05-07 14:29:49 +0100

    chore: updating inflation update fork testing

--- scripts/fork/tokenomics_update_with_timelock_account.js
+++ scripts/fork/tokenomics_update_with_timelock_account.js
@@ -28,7 +28,9 @@ async function main() {
 
     let privateKey = process.env.PRIVATE_KEY;
     let wallet = new ethers.Wallet(privateKey, provider);
-    //wallet.sendTransaction({to: timelockAddress, value: ethers.utils.parseEther("1")});
+    
+    // Fund timelock
+    await wallet.sendTransaction({to: timelockAddress, value: ethers.utils.parseEther("1")});
 
     const tokenomicsJSON = "artifacts/contracts/Tokenomics.sol/Tokenomics.json";
     let contractFromJSON = fs.readFileSync(tokenomicsJSON, "utf8");
@@ -37,7 +39,7 @@ async function main() {
 
     // Tokenomics contract instance
     const tokenomics = new ethers.Contract(tokenomicsProxyAddress, abi, signer);
-    console.log(tokenomics.address);
+    console.log("Tokenomics proxy address:", tokenomics.address);
 
     // Get bytecode
     const contractBytecode = parsedFile["bytecode"];
@@ -48,27 +50,46 @@ async function main() {
 
     const receipt = await tx.wait();
     const tokenomicsImplementationAddress = receipt.contractAddress;
-    console.log(tokenomicsImplementationAddress);
+    console.log("New tokenomics implementation address:", tokenomicsImplementationAddress);
 
     // Change tokenomics implementation
     tx = await tokenomics.changeTokenomicsImplementation(tokenomicsImplementationAddress);
     await tx.wait();
 
-    await wallet.sendTransaction({to: timelockAddress, value: ethers.utils.parseEther("1")});
+//    // Fund timelock
+//    tx = await wallet.sendTransaction({to: timelockAddress, value: ethers.utils.parseEther("1")});
+//    await tx.wait();
+//    return;
 
     // Update tokenomics inflation
+    console.log("\nUpdating tokenomics inflation");
     tx = await tokenomics.updateInflationPerSecondAndFractions(6, 9, 3, 53);
     await tx.wait();
 
     // Get inflation per second
-    const inflationPerSecond = await tokenomics.inflationPerSecond();
+    let inflationPerSecond = await tokenomics.inflationPerSecond();
     console.log("Updated inflation per second", inflationPerSecond.toString());
-    const inflationPerYear = inflationPerSecond.mul(365).mul(86400);
+    let inflationPerYear = inflationPerSecond.mul(365).mul(86400);
     console.log("Updated inflation per year", inflationPerYear.toString());
 
     // Get current effective bond
-    const effectiveBond = await tokenomics.effectiveBond();
-    const maxBond = await tokenomics.maxBond();
+    let effectiveBond = await tokenomics.effectiveBond();
+    let maxBond = await tokenomics.maxBond();
+    console.log("Updated effective bond:", effectiveBond.toString());
+    console.log("Updated max bond:", maxBond.toString());
+
+    // Update tokenomics inflation a second time (must not change values)
+    console.log("\nUpdating tokenomics inflation again without tokenomics implementation change");
+    tx = await tokenomics.updateInflationPerSecondAndFractions(6, 9, 3, 53);
+    await tx.wait();
+
+    // Get inflation per second and effective bond values
+    inflationPerSecond = await tokenomics.inflationPerSecond();
+    inflationPerYear = inflationPerSecond.mul(365).mul(86400);
+    effectiveBond = await tokenomics.effectiveBond();
+    maxBond = await tokenomics.maxBond();
+    console.log("Updated inflation per second", inflationPerSecond.toString());
+    console.log("Updated inflation per year", inflationPerYear.toString());
     console.log("Updated effective bond:", effectiveBond.toString());
     console.log("Updated max bond:", maxBond.toString());
 }
