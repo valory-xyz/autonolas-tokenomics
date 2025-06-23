c1ba5f6e0ce92166fd11ae36ee20a66dfc65894e
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-06-23 20:17:21 +0100

    tes: test correction

--- test/StakingBridging.js
+++ test/StakingBridging.js
@@ -416,11 +416,11 @@ describe("StakingBridging", async () => {
             // Process data maintenance by the owner
             const payload = ethers.utils.defaultAbiCoder.encode(["address[]", "uint256[]", "bytes32"],
                 [[stakingTarget], [stakingIncentive * 2], batchHash]);
-            await arbitrumTargetDispenserL2.processDataMaintenance(payload, true);
+            await arbitrumTargetDispenserL2.processDataMaintenance(payload, false);
 
             // Try to do it not from the owner
             await expect(
-                arbitrumTargetDispenserL2.connect(signers[1]).processDataMaintenance("0x", true)
+                arbitrumTargetDispenserL2.connect(signers[1]).processDataMaintenance("0x", false)
             ).to.be.revertedWithCustomError(arbitrumDepositProcessorL1, "OwnerOnly");
 
             // Try to redeem, but there are no funds
