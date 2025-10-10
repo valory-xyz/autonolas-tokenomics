3e020b6a8b3caf8d174b55d679e06312a2fdf623
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-10-10 15:03:47 +0100

    chore: linter

--- hardhat.config.js
+++ hardhat.config.js
@@ -35,7 +35,7 @@ const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY;
 const OPSCAN_API_KEY = process.env.OPSCAN_API_KEY;
 const BASESCAN_API_KEY = process.env.BASESCAN_API_KEY;
 const CELOSCAN_API_KEY = process.env.CELOSCAN_API_KEY;
-const isCoverage = process.env.COVERAGE === '1';
+const isCoverage = process.env.COVERAGE === "1";
 
 module.exports = {
     networks: {
