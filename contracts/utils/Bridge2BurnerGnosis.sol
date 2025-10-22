b9ac29361c832672386c2908f96fc77b2855b2b5
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-10-22 22:35:48 +0100

    feat: add Bridge2BurnerGnosis

---
+++ contracts/utils/Bridge2BurnerGnosis.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Bridge2Burner} from "./Bridge2Burner.sol";
+
+// ERC20 token interface
+interface IToken {
+    /// @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+    /// @param spender Account address that will be able to transfer tokens on behalf of the caller.
+    /// @param amount Token amount.
+    /// @return True if the function execution is successful.
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /// @dev Gets the amount of tokens owned by a specified account.
+    /// @param account Account address.
+    /// @return Amount of tokens owned.
+    function balanceOf(address account) external view returns (uint256);
+}
+
+// Bridge interface
+interface IBridge {
+    // Contract: Omnibridge Multi-Token Mediator Proxy
+    // Source: https://github.com/omni/omnibridge/blob/c814f686487c50462b132b9691fd77cc2de237d3/contracts/upgradeable_contracts/components/common/TokensRelayer.sol#L54
+    // Doc: https://docs.gnosischain.com/bridges/Token%20Bridge/omnibridge
+    function relayTokens(address token, address receiver, uint256 amount) external;
+}
+
+// @dev Reentrancy guard.
+error ReentrancyGuard();
+
+/// @dev Zero value only allowed.
+error ZeroValueOnly();
+
+/// @dev Provided zero value.
+error ZeroValue();
+
+/// @title Bridge2BurnerOptimism - Smart contract for collecting OLAS on Gnosis chain and relaying them back to L1 OLAS Burner contract.
+contract Bridge2BurnerOptimism is Bridge2Burner {
+    /// @dev Bridge2BurnerOptimism constructor.
+    /// @param _olas OLAS token address on L2.
+    /// @param _l2TokenRelayer L2 token relayer bridging contract address.
+    constructor(address _olas, address _l2TokenRelayer) Bridge2Burner(_olas, _l2TokenRelayer) {}
+
+    /// @inheritdoc Bridge2Burner
+    function relayToL1Burner(bytes memory) external payable virtual override {
+        // Reentrancy guard
+        if (_locked > 1) {
+            revert ReentrancyGuard();
+        }
+        _locked = 2;
+
+        // msg.value must be zero
+        if (msg.value > 0) {
+            revert ZeroValueOnly();
+        }
+
+        // Get OLAS balance
+        uint256 olasAmount = IToken(olas).balanceOf(address(this));
+        // Check for zero value
+        if (olasAmount == 0) {
+            revert ZeroValue();
+        }
+
+        // Approve OLAS for token relayer contract
+        IToken(olas).approve(l2TokenRelayer, olasAmount);
+
+        // Relay OLAS to L1 Burner contract
+        IBridge(l2TokenRelayer).relayTokens(olas, OLAS_BURNER, olasAmount);
+
+        _locked = 1;
+    }
+}
