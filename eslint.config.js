fa42bee1d4e0ca8b3d544677d739ba5ac23d3db6
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-10-23 23:01:57 +0100

    chore: migrating to new eslint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const globals = require("globals");
+
+module.exports = defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.commonjs,
+        },
+
+        "ecmaVersion": 13,
+        parserOptions: {},
+    },
+
+    "extends": "eslint:recommended",
+
+    "rules": {
+        "camelcase": [2, {
+            "properties": "always",
+        }],
+
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+        "no-unused-vars": "warn",
+    },
+}, globalIgnores(["artifacts/*", "cache/*", "node_modules/*", "third_party/*"]), globalIgnores(["**/coverage*", "audits/*", "lib/*"])]);
