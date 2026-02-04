ee870daf3ef9db53facb633a55f1a0620a0a431b
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2026-02-04 16:55:57 +0000

    chore: scripts for oracle and bbb deployments

---
+++ scripts/deployment/utils/deploy_03_buy_back_burner_balancer_proxy.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+
+# Get globals file
+globals="$(dirname "$0")/globals_$1.json"
+if [ ! -f $globals ]; then
+  echo "${red}!!! $globals is not found${reset}"
+  exit 0
+fi
+
+# Read variables using jq
+contractVerification=$(jq -r '.contractVerification' $globals)
+useLedger=$(jq -r '.useLedger' $globals)
+derivationPath=$(jq -r '.derivationPath' $globals)
+chainId=$(jq -r '.chainId' $globals)
+networkURL=$(jq -r '.networkURL' $globals)
+
+# Check for Polygon keys only since on other networks those are not needed
+if [ $chainId == 137 ]; then
+  API_KEY=$ALCHEMY_API_KEY_MATIC
+  if [ "$API_KEY" == "" ]; then
+      echo "set ALCHEMY_API_KEY_MATIC env variable"
+      exit 0
+  fi
+elif [ $chainId == 80002 ]; then
+    API_KEY=$ALCHEMY_API_KEY_AMOY
+    if [ "$API_KEY" == "" ]; then
+        echo "set ALCHEMY_API_KEY_AMOY env variable"
+        exit 0
+    fi
+fi
+
+olasAddress=$(jq -r '.olasAddress' $globals)
+nativeTokenAddress=$(jq -r '.nativeTokenAddress' $globals)
+balancerPriceOracleAddress=$(jq -r '.balancerPriceOracleAddress' $globals)
+balancerVaultAddress=$(jq -r '.balancerVaultAddress' $globals)
+balancerPoolId=$(jq -r '.balancerPoolId' $globals)
+maxBuyBackSlippage=$(jq -r '.maxBuyBackSlippage' $globals)
+buyBackBurnerAddress=$(jq -r '.buyBackBurnerAddress' $globals)
+
+proxyPayload=$(cast abi-encode "f(address[],bytes32,uint256)" "[$olasAddress,$nativeTokenAddress,$balancerPriceOracleAddress,$balancerVaultAddress]" $balancerPoolId $maxBuyBackSlippage)
+proxyData=$(cast calldata "initialize(bytes)" $proxyPayload)
+
+contractName="BuyBackBurnerProxy"
+contractPath="contracts/utils/$contractName.sol:$contractName"
+constructorArgs="$buyBackBurnerAddress $proxyData"
+contractArgs="$contractPath --constructor-args $constructorArgs"
+
+# Get deployer based on the ledger flag
+if [ "$useLedger" == "true" ]; then
+  walletArgs="-l --mnemonic-derivation-path $derivationPath"
+  deployer=$(cast wallet address $walletArgs)
+else
+  echo "Using PRIVATE_KEY: ${PRIVATE_KEY:0:6}..."
+  walletArgs="--private-key $PRIVATE_KEY"
+  deployer=$(cast wallet address $walletArgs)
+fi
+
+# Deployment message
+echo "${green}Deploying from: $deployer${reset}"
+echo "RPC: $networkURL"
+echo "${green}Deployment of: $contractArgs${reset}"
+
+# Deploy the contract and capture the address
+execCmd="forge create --broadcast --rpc-url $networkURL$API_KEY $walletArgs $contractArgs"
+deploymentOutput=$($execCmd)
+buyBackBurnerProxyAddress=$(echo "$deploymentOutput" | grep 'Deployed to:' | awk '{print $3}')
+
+# Get output length
+outputLength=${#buyBackBurnerProxyAddress}
+
+# Check for the deployed address
+if [ $outputLength != 42 ]; then
+  echo "${red}!!! The contract was not deployed...${reset}"
+  exit 0
+fi
+
+# Write new deployed contract back into JSON
+echo "$(jq '. += {"buyBackBurnerProxyAddress":"'$buyBackBurnerProxyAddress'"}' $globals)" > $globals
+
+# Verify contract
+if [ "$contractVerification" == "true" ]; then
+  contractParams="$buyBackBurnerProxyAddress $contractPath --constructor-args $(cast abi-encode "constructor(address,bytes)" $constructorArgs)"
+  echo "Verification contract params: $contractParams"
+
+  echo "${green}Verifying contract on Etherscan...${reset}"
+  forge verify-contract --chain-id "$chainId" --etherscan-api-key "$ETHERSCAN_API_KEY" $contractParams
+
+  blockscoutURL=$(jq -r '.blockscoutURL' $globals)
+  if [ "$blockscoutURL" != "null" ]; then
+    echo "${green}Verifying contract on Blockscout...${reset}"
+    forge verify-contract --verifier blockscout --verifier-url "$blockscoutURL/api" $contractParams
+  fi
+fi
+
+echo "${green}$contractName deployed at: $buyBackBurnerProxyAddress${reset}"
